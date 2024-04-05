0645cfb4fef201c214c5cc4e264da5d9d81a0f8f
= <alexandre.roussel.48@gmail.com>
2024-04-05 11:21:50 +0200

    Update : serveur.c

--- serveur.c
+++ serveur.c
@@ -3,39 +3,77 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <signal.h>
+
+#define TAILLE_MESS 140
+#define PORT 5000
+
+int dS;
+
+void sigint_handler(int signal) {
+  shutdown(dS, 2);
+  printf("Fin du programme\n");
+  exit(EXIT_SUCCESS);
+}
 
 int main(int argc, char *argv[]) {
   
   printf("Début programme\n");
 
-  int dS = socket(PF_INET, SOCK_STREAM, 0);
+  dS = socket(PF_INET, SOCK_STREAM, 0);
   printf("Socket Créé\n");
-
+  signal(SIGINT, sigint_handler);
 
   struct sockaddr_in ad;
   ad.sin_family = AF_INET;
   ad.sin_addr.s_addr = INADDR_ANY ;
-  ad.sin_port = htons(atoi(argv[1])) ;
+  ad.sin_port = htons(PORT) ;
   bind(dS, (struct sockaddr*)&ad, sizeof(ad)) ;
   printf("Socket Nommé\n");
 
-  listen(dS, 7) ;
+  listen(dS, 2) ;
   printf("Mode écoute\n");
 
-  struct sockaddr_in aC ;
-  socklen_t lg = sizeof(struct sockaddr_in) ;
-  int dSC = accept(dS, (struct sockaddr*) &aC,&lg) ;
-   printf("Client Connecté\n");
+  while(1) {
+    struct sockaddr_in aC1 ;
+    socklen_t lg1 = sizeof(struct sockaddr_in) ;
+    int dSC1 = accept(dS, (struct sockaddr*) &aC1,&lg1) ;
+    printf("Client 1 Connecté\n");
 
-  char msg [20] ;
-  recv(dSC, msg, sizeof(msg), 0) ;
-  printf("Message reçu : %s\n", msg) ;
-  
-  int r = 10 ;
- 
-  send(dSC, &r, sizeof(int), 0) ;
-  printf("Message Envoyé\n");
-  shutdown(dSC, 2) ; 
-  shutdown(dS, 2) ;
-  printf("Fin du programme");
+    struct sockaddr_in aC2 ;
+    socklen_t lg2 = sizeof(struct sockaddr_in) ;
+    int dSC2 = accept(dS, (struct sockaddr*) &aC2,&lg2) ;
+    printf("Client 2 Connecté\n");
+
+    char* msg = calloc(TAILLE_MESS, sizeof(char));
+    while (1) {
+      // Receive of Client 1 message
+      if (recv(dSC1, msg, TAILLE_MESS*sizeof(char), 0) > 0) {
+        printf("Message reçu Client 1: %s\n", msg);
+
+        if (strcmp(msg, "fin") == 0) {
+          break;
+        }
+
+        // Send of Client 1 message to Client 2
+        send(dSC2, msg, TAILLE_MESS*sizeof(char), 0);
+      }
+
+      // Receive of Client 2 message
+      if (recv(dSC2, msg, TAILLE_MESS*sizeof(char), 0) > 0) {
+        printf("Message reçu Client 2: %s\n", msg);
+
+        if (strcmp(msg, "fin") == 0) {
+          break;
+        }
+
+        // Send of Client 2 message to Client 1
+        send(dSC1, msg, TAILLE_MESS*sizeof(char), 0);
+      }
+    }
+    free(msg);
+    shutdown(dSC1, 2);
+    shutdown(dSC2, 2);
+  }
 }
