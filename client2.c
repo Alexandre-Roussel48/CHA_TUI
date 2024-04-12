c4c3bbbd87534b54cab2ae0290ed9f5c57a73f42
= <alexandre.roussel.48@gmail.com>
2024-04-12 14:34:26 +0200

    Ajout var globale PORT & fix cond arret fin

--- client2.c
+++ client2.c
@@ -5,6 +5,7 @@
 #include <string.h>
 
 #define TAILLE_MESS 140
+#define PORT 5000
 
 int main(int argc, char *argv[]) {
 
@@ -15,7 +16,7 @@ int main(int argc, char *argv[]) {
   struct sockaddr_in aS;
   aS.sin_family = AF_INET;
   inet_pton(AF_INET,argv[1],&(aS.sin_addr)) ;
-  aS.sin_port = htons(atoi(argv[2])) ;
+  aS.sin_port = htons(PORT) ;
   socklen_t lgA = sizeof(struct sockaddr_in) ;
   connect(dS, (struct sockaddr *) &aS, lgA) ;
   printf("Socket Connecté\n");
@@ -23,16 +24,16 @@ int main(int argc, char *argv[]) {
   int envoie = 1;
   while(envoie){
     char* messageRecu = (char*)malloc(TAILLE_MESS);
-    recv(dS, messageRecu, TAILLE_MESS, 0);
+    recv(dS, messageRecu, TAILLE_MESS*sizeof(char), 0);
     printf("Réponse reçue : %s\n", messageRecu);
 
     // L'utilisateur entre son message
     char* messageEnvoie = (char*)malloc(TAILLE_MESS);
     fgets(messageEnvoie, TAILLE_MESS, stdin);
 
-    send(dS, messageEnvoie, strlen(messageEnvoie) , 0);
+    send(dS, messageEnvoie, TAILLE_MESS*sizeof(char), 0);
     printf("Message Envoyé \n");
-    if(strcmp(messageEnvoie, "fin") == 0){envoie = 0;}
+    if(strcmp(messageEnvoie, "fin\n") == 0){envoie = 0;}
   }
 
   shutdown(dS,2) ;
