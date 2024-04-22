b10631a7180de794f329e5552c68511d234c773b
= <alexandre.roussel.48@gmail.com>
2024-04-22 12:59:18 +0200

    Update : serveur threaded

--- serveur.c
+++ serveur.c
@@ -3,9 +3,38 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
 #define TAILLE_MESS 140
 #define PORT 5001
+
+int addresses[2];
+
+void *transmission(void *t) {
+  char* msg = calloc(TAILLE_MESS, sizeof(char));
+  int i = (long)t;
+  int receiver = (i+1)%2;
+  while(1) {
+    // Reçoie un message de l'envoyeur
+    if (recv(addresses[i], msg, TAILLE_MESS*sizeof(char), 0) > 0) {
+      printf("Message reçu Client %d: %s\n",(i+1), msg);
+
+      if (strcmp(msg, "fin\n") == 0) {
+        free(msg);
+        break;
+      }
+
+      // Transmission du message vers le receveur
+      send(addresses[receiver], msg, TAILLE_MESS*sizeof(char), 0);
+    } else {
+      free(msg);
+      pthread_exit(0);
+    }
+  }
+  shutdown(addresses[0], 2);
+  shutdown(addresses[1], 2);
+  pthread_exit(0);
+}
 
 int main(int argc, char *argv[]) {
   
@@ -33,47 +62,24 @@ int main(int argc, char *argv[]) {
   printf("Mode écoute\n\n");
   printf("\x1b[0m"); // Permet de changer la couleur du texte
 
+  pthread_t threads[2];
+
   while(1) {
     socklen_t lg = sizeof(struct sockaddr_in) ;
 
     struct sockaddr_in aC1 ;
-    int dSC1 = accept(dS, (struct sockaddr*) &aC1,&lg) ;
+    addresses[0] = accept(dS, (struct sockaddr*) &aC1,&lg) ;
     printf("\x1b[34m");
     printf("Client 1 Connecté\n");
+    pthread_create(&threads[0], 0, transmission, (void*)0);
 
     struct sockaddr_in aC2 ;
-    int dSC2 = accept(dS, (struct sockaddr*) &aC2,&lg) ;
+    addresses[1] = accept(dS, (struct sockaddr*) &aC2,&lg) ;
     printf("Client 2 Connecté\n");
     printf("\x1b[0m");
+    pthread_create(&threads[1], 0, transmission, (void*)1);
 
-    char* msg = calloc(TAILLE_MESS, sizeof(char));
-    while (1) {
-      // Reçoie un message du client 1
-      if (recv(dSC1, msg, TAILLE_MESS*sizeof(char), 0) > 0) {
-        printf("Message reçu Client 1: %s\n", msg);
-
-        if (strcmp(msg, "fin\n") == 0) {
-          break;
-        }
-
-        // Transmission du message vers le client 2
-        send(dSC2, msg, TAILLE_MESS*sizeof(char), 0);
-      }
-
-      // Reçoie un message du client 2
-      if (recv(dSC2, msg, TAILLE_MESS*sizeof(char), 0) > 0) {
-        printf("Message reçu Client 2: %s\n", msg);
-
-        if (strcmp(msg, "fin\n") == 0) {
-          break;
-        }
-
-        // Transmission du message vers le client 1
-        send(dSC1, msg, TAILLE_MESS*sizeof(char), 0);
-      }
-    }
-    free(msg);
-    shutdown(dSC1, 2);
-    shutdown(dSC2, 2);
+    pthread_join(threads[0], 0);
+    pthread_join(threads[1], 0);
   }
 }
