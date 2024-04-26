aa777e495d21534630d66aeac430b25cdf15741f
KyZiiKs <86744162+Titanguy4@users.noreply.github.com>
2024-04-26 16:29:27 +0200

    add factorisation et header pour le client

---
+++ client.h
@@ -0,0 +1,20 @@
+#ifndef CLIENT
+#define CLIENT
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <pthread.h>
+
+#define TAILLE_MESS 256
+#define PORT 5001
+
+void sigint_handler(int signal);
+void init(char* address);
+void* saisie();
+void* reception();
+
+#endif
