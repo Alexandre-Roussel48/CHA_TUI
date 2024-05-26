de607f5d0f09931d87662f73e284679bde7dde10
= <alexandre.roussel.48@gmail.com>
2024-05-26 13:46:22 +0200

    update : move of code to sources

--- client.c
+++ client.c
@@ -11,10 +11,5 @@ int main(int argc, char *argv[]) {
   chat_args args;
   createChat(&args, argv[1], atoi(argv[2]));
 
-  if(launchChat(&args) == -1){
-    shutdown(args.dS,2);
-    printf("\x1b[34m");
-    printf("Fin du chat\n");
-    printf("\x1b[0m");
-  }
+  launchChat(&args);
 }
